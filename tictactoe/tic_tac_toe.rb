7c652644001d91c79609f75b661a6ec1d29c0312
Parahat Melayev <parahat@gmail.com>
2023-10-31 19:07:50 +0000

    work out diagonals dynamically

--- tictactoe/tic_tac_toe.rb
+++ tictactoe/tic_tac_toe.rb
@@ -5,31 +5,36 @@ class TicTacToe
       @state = state
     end
 
-    def getWinner()
-        horizontal_win = win?(state)
-        return horizontal_win if [0, 1].include?(horizontal_win)
+    def getWinner
+      horizontal_win = win?(state)
+      return horizontal_win if [0, 1].include?(horizontal_win)
 
-        vertical_win = win?(state.transpose)
-        return vertical_win if [0, 1].include?(vertical_win)
+      vertical_win = win?(state.transpose)
+      return vertical_win if [0, 1].include?(vertical_win)
 
-        # diagonal win
-        win?([
-            [state[0][0], state[1][1], state[2][2]],
-            [state[0][2], state[1][1], state[2][0]]
-            ])
+      # diagonal win
+      win?(diagonals)
+     end
+
+     def diagonals
+      n = state.length
+      main_diagonal = (0...n).map { |i| state[i][i] }
+      anti_diagonal = (0...n).map { |i| state[i][n-i-1] }
+      [main_diagonal, anti_diagonal]
      end
 
      def win?(rows)
-        rows.each do |row|
-            return 0 if row.uniq == [0]
-            return 1 if row.uniq == [1]
-        end
-        nil
+      rows.each do |row|
+          return 0 if row.uniq == [0]
+          return 1 if row.uniq == [1]
+      end
+
+      nil
      end
 
 end
 
-state = [[0, 0, 0], [1, 0, nil], [1, nil, nil]]
+state = [[1, 0, 0], [1, 1, nil], [0, 1, 1]]
 
 game = TicTacToe.new(state)
 winner = game.getWinner()
